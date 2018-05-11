1bafc569661086b8f0ce6cc2dd1d9301307dc15b
Unknown <luasmrks@gmail.com>
2018-05-11 18:23:13 -0500

    Add a fallback for if there manages to be 0 jetpacks and the creative tab tries to find one, or something, i guess, who cares tbh

--- src/main/java/com/blakebr0/ironjetpacks/IJCreativeTab.java
+++ src/main/java/com/blakebr0/ironjetpacks/IJCreativeTab.java
@@ -1,5 +1,6 @@
 package com.blakebr0.ironjetpacks;
 
+import com.blakebr0.ironjetpacks.item.ModItems;
 import com.blakebr0.ironjetpacks.registry.JetpackRegistry;
 
 import net.minecraft.creativetab.CreativeTabs;
@@ -13,6 +14,12 @@ public class IJCreativeTab extends CreativeTabs {
 	
 	@Override
 	public ItemStack getTabIconItem() {
-		return new ItemStack(JetpackRegistry.getInstance().getAllJetpacks().get(0).getRight());
+		JetpackRegistry registry = JetpackRegistry.getInstance();
+		
+		if (registry.getAllJetpacks().isEmpty()) {
+			return new ItemStack(ModItems.itemStrap);
+		}
+		
+		return new ItemStack(registry.getAllJetpacks().get(0).getRight());
 	}
 }
