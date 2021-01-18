8fb58611a6ab3c78c5604921ffd1e58f9c5a2277
BlakeBr0 <luasmrks@gmail.com>
2021-01-18 01:02:41 -0600

    Fix jetpack crafting component items not using the proper display name

--- src/main/java/com/blakebr0/ironjetpacks/item/ComponentItem.java
+++ src/main/java/com/blakebr0/ironjetpacks/item/ComponentItem.java
@@ -7,37 +7,31 @@ import com.blakebr0.cucumber.util.Localizable;
 import com.blakebr0.ironjetpacks.registry.Jetpack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.function.Function;
 
 public class ComponentItem extends BaseItem implements IColored, IEnableable {
-	private final String name;
+	private final Jetpack jetpack;
 	private final String type;
-	private final boolean enabled;
-	private final int color;
 	
 	public ComponentItem(Jetpack jetpack, String type, Function<Properties, Properties> properties) {
 		super(properties.compose(p -> p.rarity(jetpack.rarity)));
-		this.name = jetpack.name;
-		this.color = jetpack.color;
-		this.enabled = !jetpack.disabled;
+		this.jetpack = jetpack;
 		this.type = type;
 	}
 	
 	@Override
 	public ITextComponent getDisplayName(ItemStack stack) {
-		String name = StringUtils.capitalize(this.name.replace(" ", "_"));
-		return Localizable.of("item.ironjetpacks." + this.type).args(name).build();
+		return Localizable.of("item.ironjetpacks." + this.type).args(this.jetpack.displayName).build();
 	}
 
 	@Override
 	public int getColor(int i) {
-		return this.color;
+		return this.jetpack.color;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		return this.enabled;
+		return !this.jetpack.disabled;
 	}
 }
