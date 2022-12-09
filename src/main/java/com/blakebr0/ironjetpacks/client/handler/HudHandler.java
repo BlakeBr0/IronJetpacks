4dbec21d6320867cb9235472fe81b1c9d0b52dd6
BlakeBr0 <luasmrks@gmail.com>
2022-12-09 16:37:51 -0600

    Merge HudHelper functions into HudHandler

--- src/main/java/com/blakebr0/ironjetpacks/client/handler/HudHandler.java
+++ src/main/java/com/blakebr0/ironjetpacks/client/handler/HudHandler.java
@@ -2,9 +2,9 @@ package com.blakebr0.ironjetpacks.client.handler;
 
 import com.blakebr0.cucumber.lib.Colors;
 import com.blakebr0.ironjetpacks.IronJetpacks;
-import com.blakebr0.ironjetpacks.client.helper.HudHelper;
 import com.blakebr0.ironjetpacks.config.ModConfigs;
 import com.blakebr0.ironjetpacks.item.JetpackItem;
+import com.blakebr0.ironjetpacks.lib.ModTooltips;
 import com.blakebr0.ironjetpacks.util.JetpackUtils;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
@@ -12,6 +12,7 @@ import net.minecraft.client.gui.screens.ChatScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
@@ -27,7 +28,7 @@ public final class HudHandler {
             var item = chest.getItem();
 
             if (!chest.isEmpty() && item instanceof JetpackItem) {
-                var pos = HudHelper.getHudPos();
+                var pos = getHudPos();
                 if (pos != null) {
                     int xPos = (int) (pos.x / 0.33) - 18;
                     int yPos = (int) (pos.y / 0.33) - 78;
@@ -39,14 +40,14 @@ public final class HudHandler {
                     matrix.pushPose();
                     matrix.scale(0.33F, 0.33F, 1.0F);
                     Screen.blit(matrix, xPos, yPos, 0, 0, 28, 156, 256, 256);
-                    int i2 = HudHelper.getEnergyBarScaled(chest);
+                    int i2 = getEnergyBarScaled(chest);
                     Screen.blit(matrix, xPos, 166 - i2 + yPos - 10, 28, 156 - i2, 28, i2, 256, 256);
                     matrix.popPose();
 
-                    var fuel = Colors.GRAY + HudHelper.getFuelString(chest);
+                    var fuel = Colors.GRAY + getFuelString(chest);
                     var throttle = Colors.GRAY + "T: " + (int) (JetpackUtils.getThrottle(chest) * 100) + "%";
-                    var engine = Colors.GRAY + "E: " + HudHelper.getStatusString(JetpackUtils.isEngineOn(chest));
-                    var hover = Colors.GRAY + "H: " + HudHelper.getStatusString(JetpackUtils.isHovering(chest));
+                    var engine = Colors.GRAY + "E: " + getStatusString(JetpackUtils.isEngineOn(chest));
+                    var hover = Colors.GRAY + "H: " + getStatusString(JetpackUtils.isHovering(chest));
 
                     if (pos.side == 1) {
                         mc.font.drawShadow(matrix, fuel, pos.x - 8 - mc.font.width(fuel), pos.y - 21, 16383998);
@@ -69,6 +70,61 @@ public final class HudHandler {
         event.registerAbove(VanillaGuiOverlay.HOTBAR.id(), "jetpack_hud", HUD_OVERLAY);
     }
 
+    private static HudPos getHudPos() {
+        var window = Minecraft.getInstance().getWindow();
+        int xOffset = ModConfigs.HUD_OFFSET_X.get();
+        int yOffset = ModConfigs.HUD_OFFSET_Y.get();
+
+        return switch (ModConfigs.HUD_POSITION.get()) {
+            case 0 -> new HudPos(10 + xOffset, 30 + yOffset, 0);
+            case 1 -> new HudPos(10 + xOffset, window.getGuiScaledHeight() / 2 + yOffset, 0);
+            case 2 -> new HudPos(10 + xOffset, window.getGuiScaledHeight() - 30 + yOffset, 0);
+            case 3 -> new HudPos(window.getGuiScaledWidth() - 8 - xOffset, 30 + yOffset, 1);
+            case 4 -> new HudPos(window.getGuiScaledWidth() - 8 - xOffset, window.getGuiScaledHeight() / 2 + yOffset, 1);
+            case 5 -> new HudPos(window.getGuiScaledWidth() - 8 - xOffset, window.getGuiScaledHeight() - 30 + yOffset, 1);
+            default -> null;
+        };
+
+    }
+
+    private static int getEnergyBarScaled(ItemStack stack) {
+        var jetpack = JetpackUtils.getJetpack(stack);
+        if (jetpack.creative)
+            return 156;
+
+        var energy = JetpackUtils.getEnergyStorage(stack);
+        int i = energy.getEnergyStored();
+        int j = energy.getMaxEnergyStored();
+
+        return (int) (j != 0 && i != 0 ? (long) i * 156 / j : 0);
+    }
+
+    private static String getFuelString(ItemStack stack) {
+        var jetpack = JetpackUtils.getJetpack(stack);
+        if (jetpack.creative) {
+            return ModTooltips.INFINITE.buildString() + Colors.GRAY + " FE";
+        }
+
+        int number = JetpackUtils.getEnergyStorage(stack).getEnergyStored();
+        if (number >= 1000000000) {
+            int big = number / 1000000000;
+            int small = (number - (big * 1000000000)) / 100000000;
+            return big + ((small != 0) ? "." + small : "") + Colors.GRAY + "G FE";
+        } else if (number >= 1000000) {
+            int big = number / 1000000;
+            int small = (number - (big * 1000000)) / 100000;
+            return big + ((small != 0) ? "." + small : "") + Colors.GRAY + "M FE";
+        } else if (number >= 1000) {
+            return number / 1000 + Colors.GRAY + "k FE";
+        } else {
+            return number + Colors.GRAY + " FE";
+        }
+    }
+
+    private static String getStatusString(boolean on) {
+        return on ? Colors.GREEN + ModTooltips.ON.buildString() : Colors.RED + ModTooltips.OFF.buildString();
+    }
+
     private static boolean isVisible(Minecraft mc) {
         return ModConfigs.ENABLE_HUD.get()
                 && (ModConfigs.SHOW_HUD_OVER_CHAT.get()
@@ -76,5 +132,17 @@ public final class HudHandler {
                 && !(mc.screen instanceof ChatScreen))
                 && !mc.options.hideGui
                 && !mc.options.renderDebug;
+    }
+
+    private static class HudPos {
+        public int x;
+        public int y;
+        public int side;
+
+        public HudPos(int x, int y, int side) {
+            this.x = x;
+            this.y = y;
+            this.side = side;
+        }
     }
 }
