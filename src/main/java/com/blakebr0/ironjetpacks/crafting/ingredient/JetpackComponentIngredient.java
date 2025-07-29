b9dce454db8889e344efceb5c03d687411732fa2
Sam <livaco@livaco.dev>
2025-07-29 18:52:19 +0100

    Fix jetpack crafting ingredients being broken when power components are added

--- src/main/java/com/blakebr0/ironjetpacks/crafting/ingredient/JetpackComponentIngredient.java
+++ src/main/java/com/blakebr0/ironjetpacks/crafting/ingredient/JetpackComponentIngredient.java
@@ -1,11 +1,13 @@
 package com.blakebr0.ironjetpacks.crafting.ingredient;
 
+import com.blakebr0.ironjetpacks.IronJetpacks;
 import com.blakebr0.ironjetpacks.init.ModDataComponentTypes;
 import com.blakebr0.ironjetpacks.init.ModIngredientTypes;
 import com.blakebr0.ironjetpacks.init.ModItems;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.component.DataComponentMap;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.ItemStack;
@@ -37,7 +39,19 @@ public class JetpackComponentIngredient implements ICustomIngredient {
         if (input == null)
             return false;
 
-        return this.getItems().anyMatch(s -> ItemStack.isSameItemSameComponents(s, input));
+        DataComponentMap inputMap = input.getComponents();
+        ResourceLocation inputID = inputMap.get(ModDataComponentTypes.JETPACK_ID.get());
+        if(inputID == null)
+            return false;
+
+        return this.getItems().anyMatch(s -> {
+            DataComponentMap map = s.getComponents();
+            ResourceLocation itemID = map.get(ModDataComponentTypes.JETPACK_ID.get());
+            if(itemID == null)
+                return false;
+
+            return ItemStack.isSameItem(s, input) && itemID.getNamespace().equals(inputID.getNamespace()) && itemID.getPath().equals(inputID.getPath());
+        });
     }
 
     @Override
